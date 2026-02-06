7889297c79ba1776e13b1d6b103b5c9f899d301c
BadakCulaSatu <selatan.karimun@protonmail.com>
2026-02-06 19:07:44 +0700

    AppConfig.h

--- Source/Settings/AppConfig.h
+++ Source/Settings/AppConfig.h
@@ -1,1 +1,39 @@
+#pragma once
+#include <JuceHeader.h>
 
+class AppConfig
+{
+public:
+    struct AudioMapping
+    {
+        String mappingId;
+        String inputDeviceId;
+        String outputDeviceId;
+        bool isMuted = false;
+        float volume = 1.0f;
+        String eqPresetName;
+    };
+
+    static AppConfig& getInstance();
+
+    void load();
+    void save();
+
+    bool getLaunchAtStartup() const;
+    void setLaunchAtStartup(bool enable);
+
+    Array<AudioMapping> getAudioMappings() const;
+    void setAudioMappings(const Array<AudioMapping>& mappings);
+
+    String getLastInputDevice() const;
+    void setLastInputDevice(const String& deviceId);
+
+    String getLastOutputDevice() const;
+    void setLastOutputDevice(const String& deviceId);
+
+private:
+    AppConfig() = default;
+    PropertiesFile& getPropertiesFile();
+
+    std::unique_ptr<PropertiesFile> propertiesFile;
+};
