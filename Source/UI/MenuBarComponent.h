ad4bc8da29ddcbe0050c47accc067296a65bf935
BadakCulaSatu <selatan.karimun@protonmail.com>
2026-02-06 19:11:02 +0700

    MenuBarComponent.h

--- Source/UI/MenuBarComponent.h
+++ Source/UI/MenuBarComponent.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <JuceHeader.h>
 
+class MenuBarComponent : public SystemTrayIconComponent,
+                         private Timer
+{
+public:
+    MenuBarComponent();
+    ~MenuBarComponent() override;
+
+private:
+    void mouseDown(const MouseEvent& event) override;
+    void timerCallback() override;
+    void updateMenu();
+    void launchAtLogin(bool shouldLaunch);
+    bool isLaunchAtLoginEnabled();
+
+    std::unique_ptr<PopupMenu> menu;
+    bool isDarkMode = false;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MenuBarComponent)
+};
