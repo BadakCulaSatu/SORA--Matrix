2d35c6b9ddfe4a5f8c60e7094a0e892a3b5d9827
BadakCulaSatu <selatan.karimun@protonmail.com>
2026-02-06 19:02:56 +0700

    RoutingGraph.h

--- Source/Audio/RoutingGraph.h
+++ Source/Audio/RoutingGraph.h
@@ -1,1 +1,26 @@
+#pragma once
+#include <JuceHeader.h>
 
+class RoutingGraph : public juce::AudioProcessorGraph
+{
+public:
+    RoutingGraph();
+    ~RoutingGraph() override;
+    
+    bool addInputNode(const juce::String& inputDeviceName);
+    bool addOutputNode(const juce::String& outputDeviceName);
+    bool addRoute(int sourceNodeId, int sourceChannel, 
+                  int destNodeId, int destChannel);
+    bool addProcessingNode(juce::AudioProcessor* processor);
+    
+    void clearGraph();
+    
+    int getInputNodeId() const { return inputNodeId; }
+    int getOutputNodeId() const { return outputNodeId; }
+    
+private:
+    int inputNodeId = -1;
+    int outputNodeId = -1;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RoutingGraph)
+};
