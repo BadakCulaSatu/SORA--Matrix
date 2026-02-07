13e83eba3e281435f0f219ad8645f877d9218027
BadakCulaSatu <selatan.karimun@protonmail.com>
2026-02-07 11:45:48 +0700

    EQEditor16Band.cpp

--- Source/Plugin/EQEditor16Band.cpp
+++ Source/Plugin/EQEditor16Band.cpp
@@ -1,280 +1,269 @@
 #include "EQEditor16Band.h"
 
-class BandTabContent : public juce::Component,
-                       public juce::Slider::Listener,
-                       public juce::ComboBox::Listener
+EQEditor16Band::EQEditor16Band(ParametricEQ& eqProcessor)
+    : eqProcessor(eqProcessor)
 {
-public:
-    BandTabContent(ParametricEQ& eq, int bandIndex)
-        : eqRef(eq), bandIdx(bandIndex)
-    {
-        // Type Combo
-        typeCombo.addItem("PEAK", 1);
-        typeCombo.addItem("HPF", 2);
-        typeCombo.addItem("LPF", 3);
-        typeCombo.setSelectedId(eqRef.getBandType(bandIdx) + 1);
-        typeCombo.addListener(this);
-        addAndMakeVisible(typeCombo);
-        
-        // Freq
-        freqSlider.setRange(0.0, 22000.0, 1.0);
-        freqSlider.setSkewFactorFromMidPoint(1000.0);
-        freqSlider.setTextValueSuffix(" Hz");
-        freqSlider.setValue(eqRef.getBandFreq(bandIdx));
-        freqSlider.addListener(this);
-        addAndMakeVisible(freqSlider);
-        
-        freqLabel.setText("Frequency", juce::dontSendNotification);
-        addAndMakeVisible(freqLabel);
-        
-        // Gain (hide untuk HPF/LPF)
-        gainSlider.setRange(-22.0, 22.0, 0.1);
-        gainSlider.setTextValueSuffix(" dB");
-        gainSlider.setValue(eqRef.getBandGain(bandIdx));
-        gainSlider.addListener(this);
-        addAndMakeVisible(gainSlider);
-        
-        gainLabel.setText("Gain", juce::dontSendNotification);
-        addAndMakeVisible(gainLabel);
-        
-        // Q Controls
-        qSlider.setRange(0.4, 40.0, 0.1);
-        qSlider.setSkewFactorFromMidPoint(1.0);
-        qSlider.setValue(eqRef.getBandQ(bandIdx));
-        qSlider.addListener(this);
-        addAndMakeVisible(qSlider);
-        
-        qCombo.addItem("6 dB/oct", 1);
-        qCombo.addItem("12 dB/oct", 2);
-        qCombo.addItem("24 dB/oct", 3);
-        qCombo.addItem("48 dB/oct", 4);
-        qCombo.setSelectedId(2);
-        qCombo.addListener(this);
-        addAndMakeVisible(qCombo);
-        
-        qLabel.setText("Q/Slope", juce::dontSendNotification);
-        addAndMakeVisible(qLabel);
-        
-        updateControlsVisibility();
-    }
+    setSize(800, 600);
     
-    void resized() override
-    {
-        auto bounds = getLocalBounds().reduced(10);
-        
-        auto topRow = bounds.removeFromTop(30);
-        typeCombo.setBounds(topRow.removeFromLeft(100));
-        topRow.removeFromLeft(20);
-        qLabel.setBounds(topRow.removeFromLeft(80));
-        qCombo.setBounds(topRow.removeFromLeft(100));
-        qSlider.setBounds(topRow.removeFromLeft(250));
-        
-        bounds.removeFromTop(20);
-        
-        freqLabel.setBounds(bounds.removeFromTop(20));
-        freqSlider.setBounds(bounds.removeFromTop(30));
-        
-        bounds.removeFromTop(10);
-        
-        gainLabel.setBounds(bounds.removeFromTop(20));
-        gainSlider.setBounds(bounds.removeFromTop(30));
-    }
+    addAndMakeVisible(viewport);
+    viewport.setViewedComponent(&bandsContainer, false);
     
-    void updateControlsVisibility()
-    {
-        bool isPeak = eqRef.getBandType(bandIdx) == ParametricEQ::PEAK;
-        gainSlider.setVisible(isPeak);
-        gainLabel.setVisible(isPeak);
-        qSlider.setVisible(isPeak);
-        qCombo.setVisible(!isPeak);
-        
-        if (!isPeak)
-        {
-            float q = eqRef.getBandQ(bandIdx);
-            int selected = 2;
-            if (q >= 0.6f) selected = 1;
-            else if (q <= 0.25f) selected = 4;
-            else if (q <= 0.35f) selected = 3;
-            qCombo.setSelectedId(selected);
-        }
-    }
-    
-private:
-    void sliderValueChanged(juce::Slider* slider) override
-    {
-        if (slider == &freqSlider)
-            eqRef.setBandParameters(bandIdx, (float)freqSlider.getValue(), 
-                                  eqRef.getBandGain(bandIdx), eqRef.getBandQ(bandIdx));
-        else if (slider == &gainSlider)
-            eqRef.setBandParameters(bandIdx, eqRef.getBandFreq(bandIdx),
-                                  (float)gainSlider.getValue(), eqRef.getBandQ(bandIdx));
-        else if (slider == &qSlider)
-            eqRef.setBandParameters(bandIdx, eqRef.getBandFreq(bandIdx),
-                                  eqRef.getBandGain(bandIdx), (float)qSlider.getValue());
-    }
-    
-    void comboBoxChanged(juce::ComboBox* comboBox) override
-    {
-        if (comboBox == &typeCombo)
-        {
-            auto type = (ParametricEQ::FilterType)(typeCombo.getSelectedId() - 1);
-            eqRef.setBandType(bandIdx, type);
-            updateControlsVisibility();
-        }
-        else if (comboBox == &qCombo)
-        {
-            if (eqRef.getBandType(bandIdx) != ParametricEQ::PEAK)
-            {
-                float qValue = 0.5f;
-                switch (qCombo.getSelectedId())
-                {
-                    case 1: qValue = 0.707f; break;
-                    case 2: qValue = 0.5f; break;
-                    case 3: qValue = 0.3f; break;
-                    case 4: qValue = 0.2f; break;
-                }
-                eqRef.setBandParameters(bandIdx, eqRef.getBandFreq(bandIdx),
-                                      eqRef.getBandGain(bandIdx), qValue);
-            }
-        }
-    }
-    
-    ParametricEQ& eqRef;
-    int bandIdx;
-    
-    juce::ComboBox typeCombo;
-    juce::Slider freqSlider, gainSlider, qSlider;
-    juce::ComboBox qCombo;
-    juce::Label freqLabel, gainLabel, qLabel;
-};
+    updateBandControls();
+}
 
-// Main Editor
-EQEditor16Band::EQEditor16Band(ParametricEQ& eq)
-    : eqRef(eq), tabbedComponent(juce::TabbedButtonBar::TabsAtTop)
+EQEditor16Band::~EQEditor16Band()
 {
-    // Create tabs for 16 bands
-    for (int i = 0; i < 16; ++i)
+}
+
+void EQEditor16Band::paint(juce::Graphics& g)
+{
+    g.fillAll(juce::Colour(30, 30, 32));
+    
+    // Draw frequency grid
+    g.setColour(juce::Colour(60, 60, 62));
+    const int numVerticalLines = 10;
+    const int numHorizontalLines = 8;
+    
+    for (int i = 1; i < numVerticalLines; ++i)
     {
-        createBandTab(i);
+        float x = getWidth() * i / (float)numVerticalLines;
+        g.drawVerticalLine((int)x, 0, getHeight());
     }
     
-    addAndMakeVisible(tabbedComponent);
+    for (int i = 1; i < numHorizontalLines; ++i)
+    {
+        float y = getHeight() * i / (float)numHorizontalLines;
+        g.drawHorizontalLine((int)y, 0, getWidth());
+    }
     
-    // Preset Management
-    presetCombo.addListener(this);
-    addAndMakeVisible(presetCombo);
+    // Draw center line
+    g.setColour(juce::Colour(80, 80, 82));
+    float centerY = getHeight() * 0.5f;
+    g.drawHorizontalLine((int)centerY, 0, getWidth());
     
-    loadButton.setButtonText("Load");
-    loadButton.addListener(this);
-    addAndMakeVisible(loadButton);
+    // Draw frequency response curve
+    g.setColour(juce::Colours::cyan);
+    g.strokePath(frequencyResponsePath, juce::PathStrokeType(2.0f));
     
-    saveButton.setButtonText("Save");
-    saveButton.addListener(this);
-    addAndMakeVisible(saveButton);
-    
-    deleteButton.setButtonText("Delete");
-    deleteButton.addListener(this);
-    addAndMakeVisible(deleteButton);
-    
-    presetNameEditor.setText("New Preset");
-    addAndMakeVisible(presetNameEditor);
-    
-    updatePresetList();
-    
-    setSize(600, 500);
+    // Draw nodes
+    g.setColour(juce::Colours::white);
+    for (const auto& pos : nodePositions)
+    {
+        g.fillEllipse(pos.x - 4, pos.y - 4, 8, 8);
+        g.drawEllipse(pos.x - 6, pos.y - 6, 12, 12, 1.0f);
+    }
 }
 
 void EQEditor16Band::resized()
 {
-    auto bounds = getLocalBounds().reduced(10);
+    viewport.setBounds(getLocalBounds().reduced(10));
+    bandsContainer.setSize(getWidth() - 20, 400);
     
-    auto presetArea = bounds.removeFromTop(30);
-    presetCombo.setBounds(presetArea.removeFromLeft(200));
-    presetArea.removeFromLeft(10);
-    presetNameEditor.setBounds(presetArea.removeFromLeft(150));
-    presetArea.removeFromLeft(10);
-    loadButton.setBounds(presetArea.removeFromLeft(60));
-    presetArea.removeFromLeft(5);
-    saveButton.setBounds(presetArea.removeFromLeft(60));
-    presetArea.removeFromLeft(5);
-    deleteButton.setBounds(presetArea.removeFromLeft(60));
-    
-    bounds.removeFromTop(10);
-    tabbedComponent.setBounds(bounds);
-}
-
-void EQEditor16Band::createBandTab(int bandIndex)
-{
-    auto* tabContent = new BandTabContent(eqRef, bandIndex);
-    bandTabs.add(tabContent);
-    tabbedComponent.addTab("Band " + juce::String(bandIndex + 1), 
-                          juce::Colours::darkgrey, tabContent, true);
-}
-
-void EQEditor16Band::updatePresetList()
-{
-    presetCombo.clear();
-    presetCombo.addItem("- Select Preset -", 1);
-    auto presets = eqRef.getPresetList();
-    for (int i = 0; i < presets.size(); ++i)
-        presetCombo.addItem(presets[i], i + 2);
-    presetCombo.setSelectedId(1);
-}
-
-void EQEditor16Band::saveCurrentPreset()
-{
-    auto name = presetNameEditor.getText();
-    if (name.isNotEmpty())
+    // Position band controls
+    int y = 10;
+    for (auto* control : bandControls)
     {
-        eqRef.savePreset(name);
-        updatePresetList();
-        presetNameEditor.setText("");
+        control->setBounds(10, y, bandsContainer.getWidth() - 20, 60);
+        y += 70;
     }
+    
+    bandsContainer.setSize(bandsContainer.getWidth(), y + 10);
 }
 
-void EQEditor16Band::loadSelectedPreset()
+void EQEditor16Band::mouseDoubleClick(const juce::MouseEvent& event)
 {
-    int id = presetCombo.getSelectedId();
-    if (id > 1)
+    auto localPos = event.getPosition().toFloat();
+    addBandAtPoint(localPos);
+}
+
+void EQEditor16Band::addBandAtPoint(juce::Point<float> position)
+{
+    // Convert position to EQ parameters
+    float freq = std::pow(10.0f, position.x / (float)getWidth() * 4.0f + 1.0f); // 10Hz - 22kHz
+    float gain = (0.5f - position.y / (float)getHeight()) * 44.0f - 22.0f; // -22 to +22 dB
+    
+    // Limit to specified ranges
+    freq = juce::jlimit(0.0f, 22000.0f, freq);
+    gain = juce::jlimit(-22.0f, 22.0f, gain);
+    
+    ParametricEQ::EQBand newBand;
+    newBand.type = ParametricEQ::EQBand::PEAK;
+    newBand.frequency = freq;
+    newBand.gain = gain;
+    newBand.q = 1.0f;
+    newBand.slope = 12;
+    newBand.nodePosition = position;
+    
+    eqProcessor.addBand(newBand);
+    updateBandControls();
+    repaint();
+}
+
+void EQEditor16Band::deleteBand(int index)
+{
+    eqProcessor.removeBand(index);
+    updateBandControls();
+    repaint();
+}
+
+void EQEditor16Band::updateBandControls()
+{
+    bandControls.clear();
+    
+    const auto& bands = eqProcessor.getBands();
+    for (int i = 0; i < bands.size(); ++i)
     {
-        auto name = presetCombo.getItemText(id - 1);
-        eqRef.loadPreset(name);
+        auto* control = new BandControl(i, eqProcessor);
+        bandControls.add(control);
+        bandsContainer.addAndMakeVisible(control);
+    }
+    
+    resized();
+}
+
+void EQEditor16Band::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+    repaint();
+}
+
+// BandControl implementation
+EQEditor16Band::BandControl::BandControl(int index, ParametricEQ& eq)
+    : bandIndex(index), eqProcessor(eq)
+{
+    typeCombo = std::make_unique<juce::ComboBox>();
+    typeCombo->addItem("PEAK", 1);
+    typeCombo->addItem("HPF", 2);
+    typeCombo->addItem("LPF", 3);
+    typeCombo->setSelectedId(1, juce::dontSendNotification);
+    typeCombo->onChange = [this] { updateBand(); };
+    addAndMakeVisible(typeCombo.get());
+    
+    freqSlider = std::make_unique<juce::Slider>(juce::Slider::LinearHorizontal, juce::Slider::TextBoxBelow);
+    freqSlider->setRange(0.0, 22000.0, 1.0);
+    freqSlider->setSkewFactorFromMidPoint(1000.0);
+    freqSlider->onValueChange = [this] { updateBand(); };
+    addAndMakeVisible(freqSlider.get());
+    
+    gainSlider = std::make_unique<juce::Slider>(juce::Slider::LinearVertical, juce::Slider::TextBoxLeft);
+    gainSlider->setRange(-22.0, 22.0, 0.1);
+    gainSlider->onValueChange = [this] { updateBand(); };
+    addAndMakeVisible(gainSlider.get());
+    
+    qSlider = std::make_unique<juce::Slider>(juce::Slider::LinearHorizontal, juce::Slider::TextBoxBelow);
+    qSlider->setRange(0.4, 40.0, 0.1);
+    qSlider->onValueChange = [this] { updateBand(); };
+    addAndMakeVisible(qSlider.get());
+    
+    slopeCombo = std::make_unique<juce::ComboBox>();
+    slopeCombo->addItem("6 dB/oct", 6);
+    slopeCombo->addItem("12 dB/oct", 12);
+    slopeCombo->addItem("24 dB/oct", 24);
+    slopeCombo->addItem("48 dB/oct", 48);
+    slopeCombo->setSelectedId(12, juce::dontSendNotification);
+    slopeCombo->onChange = [this] { updateBand(); };
+    addAndMakeVisible(slopeCombo.get());
+    
+    deleteButton = std::make_unique<juce::TextButton>("X");
+    deleteButton->onClick = [this] {
+        if (auto* editor = findParentComponentOfClass<EQEditor16Band>())
+            editor->deleteBand(bandIndex);
+    };
+    addAndMakeVisible(deleteButton.get());
+    
+    // Labels
+    addAndMakeVisible(freqLabel);
+    freqLabel.setText("Freq (Hz)", juce::dontSendNotification);
+    freqLabel.setJustificationType(juce::Justification::centred);
+    
+    addAndMakeVisible(gainLabel);
+    gainLabel.setText("Gain (dB)", juce::dontSendNotification);
+    gainLabel.setJustificationType(juce::Justification::centred);
+    
+    addAndMakeVisible(qLabel);
+    qLabel.setText("Q", juce::dontSendNotification);
+    qLabel.setJustificationType(juce::Justification::centred);
+    
+    addAndMakeVisible(slopeLabel);
+    slopeLabel.setText("Slope", juce::dontSendNotification);
+    slopeLabel.setJustificationType(juce::Justification::centred);
+    
+    updateFromBand();
+}
+
+EQEditor16Band::BandControl::~BandControl()
+{
+}
+
+void EQEditor16Band::BandControl::paint(juce::Graphics& g)
+{
+    g.fillAll(juce::Colour(40, 40, 42));
+    g.setColour(juce::Colours::white);
+    g.drawRect(getLocalBounds(), 1);
+}
+
+void EQEditor16Band::BandControl::resized()
+{
+    auto bounds = getLocalBounds().reduced(5);
+    
+    typeCombo->setBounds(bounds.removeFromTop(20));
+    bounds.removeFromTop(5);
+    
+    deleteButton->setBounds(bounds.removeFromRight(30).reduced(2));
+    
+    auto freqBounds = bounds.removeFromTop(40);
+    freqLabel.setBounds(freqBounds.removeFromTop(15));
+    freqSlider->setBounds(freqBounds);
+    
+    auto gainBounds = bounds.removeFromRight(60);
+    gainLabel.setBounds(gainBounds.removeFromTop(15));
+    gainSlider->setBounds(gainBounds);
+    
+    auto qBounds = bounds.removeFromTop(40);
+    qLabel.setBounds(qBounds.removeFromTop(15));
+    qSlider->setBounds(qBounds);
+    
+    auto slopeBounds = bounds.removeFromTop(40);
+    slopeLabel.setBounds(slopeBounds.removeFromTop(15));
+    slopeCombo->setBounds(slopeBounds);
+}
+
+void EQEditor16Band::BandControl::updateFromBand()
+{
+    const auto& bands = eqProcessor.getBands();
+    if (bandIndex >= 0 && bandIndex < bands.size())
+    {
+        const auto& band = bands[bandIndex];
         
-        // Refresh all tabs
-        for (int i = 0; i < 16; ++i)
+        typeCombo->setSelectedId(band.type + 1, juce::dontSendNotification);
+        freqSlider->setValue(band.frequency, juce::dontSendNotification);
+        gainSlider->setValue(band.gain, juce::dontSendNotification);
+        qSlider->setValue(band.q, juce::dontSendNotification);
+        slopeCombo->setSelectedId(band.slope, juce::dontSendNotification);
+        
+        // Enable/disable controls based on band type
+        bool isFilter = (band.type == ParametricEQ::EQBand::HPF || 
+                        band.type == ParametricEQ::EQBand::LPF);
+        
+        gainSlider->setEnabled(!isFilter);
+        qSlider->setEnabled(!isFilter);
+        slopeCombo->setEnabled(isFilter);
+        
+        // For filters, set gain to 0
+        if (isFilter)
         {
-            if (auto* tab = dynamic_cast<BandTabContent*>(bandTabs[i]))
-            {
-                // Update UI components dengan nilai baru
-                // (Perlu tambah method refresh di BandTabContent)
-            }
+            gainSlider->setValue(0.0, juce::dontSendNotification);
         }
     }
 }
 
-void EQEditor16Band::deleteSelectedPreset()
+void EQEditor16Band::BandControl::updateBand()
 {
-    int id = presetCombo.getSelectedId();
-    if (id > 1)
-    {
-        auto name = presetCombo.getItemText(id - 1);
-        eqRef.getPresetFile(name).deleteFile();
-        updatePresetList();
-    }
-}
-
-void EQEditor16Band::sliderValueChanged(juce::Slider*) {}
-void EQEditor16Band::comboBoxChanged(juce::ComboBox* combo)
-{
-    if (combo == &presetCombo)
-    {
-        // Bisa tambah auto-load jika mau
-    }
-}
-
-void EQEditor16Band::buttonClicked(juce::Button* button)
-{
-    if (button == &loadButton) loadSelectedPreset();
-    else if (button == &saveButton) saveCurrentPreset();
-    else if (button == &deleteButton) deleteSelectedPreset();
+    ParametricEQ::EQBand band;
+    band.type = static_cast<ParametricEQ::EQBand::Type>(typeCombo->getSelectedId() - 1);
+    band.frequency = (float)freqSlider->getValue();
+    band.gain = (float)gainSlider->getValue();
+    band.q = (float)qSlider->getValue();
+    band.slope = slopeCombo->getSelectedId();
+    
+    eqProcessor.updateBand(bandIndex, band);
 }
